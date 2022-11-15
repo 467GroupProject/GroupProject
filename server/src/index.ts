9488ec484625a9491a71b847d530225ef2a7ad22
mmang126 <mmang126@yahoo.com>
2022-11-15 10:36:28 -0600

    Use cors and morgan

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,17 @@
  */
 
 import express from 'express'
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 const port: number = 3000;
 
 app.use(express.json());
+// for testing, see what is hitting our server
+app.use(morgan('combined'));
+// for deployment in real world environment
+app.use(cors());
 
 const server = app.listen(process.env.PORT || port, () =>
   console.log(`listening on port ${port}`),
