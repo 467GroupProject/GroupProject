587cc3760805cd668fa19146d426479a1e99ee62
mmang126 <mmang126@yahoo.com>
2022-11-15 13:30:21 -0600

    Vue all prodcuts

---
+++ client/src/views/ProductView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="products">
+        <secion v-if="errored">
+            <p>Sorry, something went wrong.</p>
+        </secion>
+        <section v-else>
+            <div v-for="(product, index) in products" v-bind:key="product">
+                <img v-if="product.pictureURL" :src="product.pictureURL">
+                <span class="lighten">
+                    <span></span> ${{ product.price }} {{ product.weight }}lb quantity {{ quantity[index].quantity }}
+                </span>
+            </div> 
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+import authenticationService
+ from '@/services/authenticationService';
+ export default{
+    data: () => ({
+        products: [],
+        quantity: [],
+        errored: false,
+        width: 150
+    }),
+    mounted() {
+        // get all products from legacy database
+        authenticationService.product()
+        .then(response => (this.products = response.data))
+        .catch(error => {
+            console.log(error);
+            this.errored = true;
+        }),
+        // get all inventory from internal database
+        authenticationService.inventory()
+        .then(rensponse => (this.quantity = rensponse.data))
+        .catch(error => {
+            console.log(error);
+            this.errored = true;
+        });
+    }
+};
+</script>
