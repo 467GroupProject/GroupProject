80411bdf0fdc7b5563423bc2e59bb030db581d9c
mmang126 <61669838+mmang126@users.noreply.github.com>
2022-11-24 12:06:10 -0600

    Try cath

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,28 +24,43 @@ app.use(cors());
 
 // qeury legacy database to get all products
 app.get('/products', async(req, res) => {
-  const allParts = await legacyDB.parts.findMany();
-  res.send(allParts);
+  try{
+    const allParts = await legacyDB.parts.findMany();
+    res.send(allParts);
+  }
+  catch(error){
+    res.send(error);
+  }
 });
 
 // query internal database to get all inventory
 app.get('/iventory', async(req, res) => {
-  const allInventory = await internalDB.inventory.findMany();
-  res.send(allInventory);
+  try {
+    const allInventory = await internalDB.inventory.findMany();
+    res.send(allInventory);
+  }
+  catch(error){
+    res.send(error);
+  }
 });
 
 app.post('/orders', async(req, res) => {
-  const orders = await internalDB.orders.create({
-    data: {
-      customer_name: req.body.customer_name,
-      customer_email: req.body.customer_email,
-      customer_address: req.body.customer_address,
-      total_amount: req.body.total_amount,
-      total_weight: req.body.status,
-      date: req.body.date
-    }
-  })
-  res.send(orders)
+  try{
+    const orders = await internalDB.orders.create({
+      data: {
+        customer_name: req.body.customer_name,
+        customer_email: req.body.customer_email,
+        customer_address: req.body.customer_address,
+        total_amount: req.body.total_amount,
+        total_weight: req.body.total_weight,
+        date: req.body.date
+      }
+    })
+    res.send(orders)
+  }
+  catch(error){
+    res.send(error);
+  }
 })
 
 app.post('/register', async(req, res) => {
@@ -55,15 +70,20 @@ app.post('/register', async(req, res) => {
 })
 
 app.post('/update', async(req, res) => {
-  const updateQty = await internalDB.inventory.update({
-    where: {
-      id: req.body.id
-    },
-    data: {
-      quantity: req.body.quantity
-    }
-  })
-  res.send(updateQty);
+  try{
+    const updateQty = await internalDB.inventory.update({
+      where: {
+        id: req.body.id
+      },
+      data: {
+        quantity: req.body.quantity
+      }
+    })
+    res.send(updateQty);
+  }
+  catch(error){
+    res.send(error);
+  }
 })
 
 const server = app.listen(process.env.PORT || port, () =>
