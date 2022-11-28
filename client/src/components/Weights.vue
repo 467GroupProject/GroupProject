d2f6b24401279ad9c80ffe7f62a673ebfc375e5b
mmang126 <mmang126@yahoo.com>
2022-11-28 09:39:22 -0600

    Admin

---
+++ client/src/components/Weights.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-container fluid class="text-center">
+        <v-card>
+            <v-card-title>Weight Brackets to Calculate Shipping</v-card-title>
+            <v-table>
+                <tbody>
+                    <tr class="trow">
+                        <td></td>
+                        <td class="text-left">
+                        Weight range</td>
+                        <td class="text-left">
+                        Price</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr class="trow">
+                        <td></td>
+                    </tr>
+                    <tr class="trow">
+                        <td></td>
+                        <td class="text-left">
+                        Weight</td>
+                        <td>
+                            <v-text-field
+                            class="new"></v-text-field>
+                        </td>
+                        <td class="text-left">
+                        Cost</td>
+                        <td>
+                            <v-text-field
+                            class="new"></v-text-field>
+                        </td>
+                    </tr>
+                </tbody>
+            </v-table>
+        </v-card>
+
+    </v-container>
+
+</template>
+
+<script lang="ts">
+import { useWeightStore } from '@/stores/weightStore';
+
+export default{
+    setup(){
+        const weightStore = useWeightStore();
+        weightStore.fill();
+
+        return { weightStore }
+    }
+}
+</script>
+
+<style scoped>
+    .new{
+        height: 50px;
+        width: 60px;
+    }
+    .trow{
+        height: 70px;
+    }
+</style>
