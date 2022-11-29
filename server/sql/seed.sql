a37fd271343d9749adc07edac62bbcbff6c49c6b
TheJaywilk <jlloydwilk@gmail.com>
2022-11-29 10:37:32 -0600

    capitalization fixes
    Fixed capitalization changes I made so that script should work on linux and mac. Sorry

--- server/sql/seed.sql
+++ server/sql/seed.sql
@@ -1,35 +1,35 @@
 -- sql to seed demo data into internal database
 
-LOAD DATA LOCAL INFILE 'C:/Users/dotbo/OneDrive/Desktop/Class Files/CSCI 467 - Intro Software Engineering/git-repo/GroupProject/server/sql/weight.csv'
-INTO TABLE weight
+LOAD DATA LOCAL INFILE 'weight.csv'
+INTO TABLE Weight
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:/Users/dotbo/OneDrive/Desktop/Class Files/CSCI 467 - Intro Software Engineering/git-repo/GroupProject/server/sql/inventory.csv' 
-INTO TABLE inventory
+LOAD DATA LOCAL INFILE 'inventory.csv' 
+INTO TABLE Inventory
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:/Users/dotbo/OneDrive/Desktop/Class Files/CSCI 467 - Intro Software Engineering/git-repo/GroupProject/server/sql/user.csv' 
-INTO TABLE user
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE 'C:/Users/dotbo/OneDrive/Desktop/Class Files/CSCI 467 - Intro Software Engineering/git-repo/GroupProject/server/sql/orders.csv' 
-INTO TABLE orders
+LOAD DATA LOCAL INFILE 'user.csv' 
+INTO TABLE User
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'C:/Users/dotbo/OneDrive/Desktop/Class Files/CSCI 467 - Intro Software Engineering/git-repo/GroupProject/server/sql/order_product.csv' 
-INTO TABLE order_product
+LOAD DATA LOCAL INFILE 'orders.csv' 
+INTO TABLE Orders
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'order_product.csv' 
+INTO TABLE Order_Product
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
