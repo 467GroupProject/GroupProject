6598ccd593193082924afc64c7235d2a882c9d8c
TheJaywilk <jlloydwilk@gmail.com>
2022-11-29 19:13:04 -0600

    Orders wip
    Working on confirmation dialog details

--- client/src/components/Orders.vue
+++ client/src/components/Orders.vue
@@ -14,11 +14,66 @@
                         <td>{{ o.id }}</td>
                         <td>${{ o.total_amount }}</td>
                         <td>{{ o.total_weight }} lb(s)</td>
-                        <td><v-btn @click="onClick(o.id)">Complete Order</v-btn></td>
+                        <td><v-btn @click="onClick(o)">Complete Order</v-btn></td>
                     </tr>
                 </tbody>
             </v-table>
         </v-card>
+
+
+        <v-dialog v-model="close_confirmation_popup" width="40%">
+                <v-card>
+                    <v-container fluid>
+                        <v-card-title class="text-center">
+                        Order Filling: {{curr_order.id}}
+                        </v-card-title>
+
+                        <v-card-text>
+                            {{curr_order}}
+
+                            <div class="text-left">
+                                <v-card-h1 class="font-weight-bold">
+                                    Packing List:
+                                </v-card-h1>
+                                <p>
+
+                                </p>
+                            </div>
+                               
+                                
+                            <div class="text-left">
+                                <v-card-h1 class="font-weight-bold">
+                                    Invoice:
+                                </v-card-h1>
+                                <p>
+                                    
+                                </p>
+                            </div>
+
+
+                            <div class="text-left">
+                                <v-card-h1 class="font-weight-bold">
+                                    Shipping Label:
+                                </v-card-h1>
+                                <p class="text-sm-left">
+                                    {{curr_order.customer_name}}<br/>
+                                    {{curr_order.customer_address}}<br/>
+                                    Confirmation email will be sent to {{curr_order.customer_email}}
+                                </p>
+                            </div>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-btn width="50%" color="green"  @click="test_dialog2 = true"> Confirm & Close </v-btn>
+                            <v-btn width="50%" color="red"  @click="close_confirmation_popup = false">Cancel</v-btn>
+                        </v-card-actions>
+                    </v-container>
+
+                    
+                </v-card>
+
+        </v-dialog>
+        
     </v-container>
     
 </template>
@@ -34,17 +89,22 @@ export default{
         return { orderStore };
     },
     methods: {
-        async onClick(pid: Number){
-            await(authenticationServie.completeOrder({
-                    id: pid,
-                    status: 'closed'
-                })
-            )
+        async onClick(order){
+            this.close_confirmation_popup = true;
+            this.curr_order = order;
+            //await(authenticationServie.completeOrder({
+            //        id: pid,
+            //        status: 'closed'
+            //    })
+            //)
         }
     },
     data() {
         return{
-            orderProd: []
+            orderProd: [],
+            close_confirmation_popup: false,
+            test_dialog2: false,
+            curr_order: null,
         }
     },
     mounted() {
